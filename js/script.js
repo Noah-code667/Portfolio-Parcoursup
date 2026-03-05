7fed4952903640f712135985e603b659259efe60
NoahHtml-dev <pylab.cyber@gmail.com>
2026-03-05 22:09:29 +0100

    modification sur tout les codes pour avoir une carte centre et un effet de defillement horizontale sur les cartes de projets

--- js/script.js
+++ js/script.js
@@ -1,17 +1,69 @@
 const container = document.getElementById("projectsScroll")
+const cards = document.querySelectorAll(".project-card")
+let index = 0
+
+function updateCenterCard() {
+
+    const cardWidth = cards[0].offsetWidth + 20
+    container.scrollTo({
+        left: index * cardWidth,
+        behavior: "smooth"
+    })
+
+    cards.forEach((card, i) => {
+
+        if (i === index) {
+            card.classList.add("active")
+        } 
+        else {
+            card.classList.remove("active")
+        }
+
+    })
+
+}
 
 container.addEventListener("wheel", (evt) => {
 
-evt.preventDefault()
+    evt.preventDefault()
 
-container.scrollLeft += evt.deltaY
+    if (evt.deltaY > 0) {
+
+        if (index < cards.length - 1) {
+            index++
+        }
+
+    } 
+    else {
+
+        if (index > 0) {
+            index--
+        }
+
+    }
+
+    updateCenterCard()
 
 })
 
 document.getElementById("scrollLeft").onclick = () => {
-container.scrollLeft -= 300
+
+    if (index > 0) {
+        index--
+    }
+
+    updateCenterCard()
+
 }
 
 document.getElementById("scrollRight").onclick = () => {
-container.scrollLeft += 300
+
+    if (index < cards.length - 1) {
+        index++
+    }
+
+    updateCenterCard()
+
 }
+
+updateCenterCard()
